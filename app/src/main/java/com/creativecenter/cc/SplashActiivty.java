05fea0c3b87be43ecdba5ed64c6bf8a76f584bc8
ngpdeveloper <ngp.developer@gmail.com>
2018-01-07 12:07:39 +0530

    test commit

--- app/src/main/java/com/creativecenter/cc/SplashActiivty.java
+++ app/src/main/java/com/creativecenter/cc/SplashActiivty.java
@@ -2,6 +2,7 @@ package com.creativecenter.cc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActiivty extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class SplashActiivty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Log.v("result","test git");
     }
 }
